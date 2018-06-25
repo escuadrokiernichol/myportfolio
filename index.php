f7ff8412d9be8324d1ff33707caf8592a92d8be1
unknown <escuadro.kiernichol@gmail.com>
2018-06-26 04:47:59 +0800

    portfolio

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Home</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/progress-circle.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<section id="head">
+		<div class="grid-container" id="headGrid">
+			<div class="grid-item" id="headLeft">
+				<img src="./assets/image/me.jpg" alt="Kier Escuadro" id="hlImg">
+			</div>
+			<div class="grid-item" id="headRight">
+				<div class="grid-container name" id="headGridtop">
+					<div class="grid-item">
+						<h1>Kier Nichol Escuadro</h1>
+					</div>
+					<div class="grid-item text-center" id="downloadCV">
+						<span><i class="fas fa-download fa-2x"></i></span>
+					</div>
+				</div>
+				<div class="grid-container" id="profession">
+					<h3>Web Developer</h3>
+				</div>
+				<div class="grid-container text-center" id="headGridbott">
+					<div class="grid-item" id="cv"><a href="" class="homeLinkout"><span><i class="fas fa-user-graduate"></i></span> RESUME</a></div>
+					<div class="grid-item" id="portfolio"><a href="" class="homeLinkout"><span><i class="fas fa-toolbox"></i></span> PORTFOLIO</a></div>
+					<!-- Add linkout here -->
+				</div>
+			</div>
+		</div>
+	</section>	
+	<section id="body">
+		<div class="grid-container" id="bodySection">
+			<div class="grid-item">7</div>
+			<div class="grid-item" id="bodyRight">
+				<div class="grid-container" id="bodyPersonalHead">
+					<div class="grid-item" id="bodyPerHead">
+						<h3>PERSONAL INFO</h3>
+					</div>
+					<div class="grid-item">
+						<div class="grid-container" id="bodyPersonalDetails">
+							<div class="grid-item" id="bodyPerInfo">
+								<p>NAME:</p>
+								<p>BIRTH DATE:</p>
+								<p>ADDRESS:</p>
+								<p>PHONE:</p>
+								<p>EMAIL:</p>
+							</div>					
+							<div class="grid-item">
+								<p>Kier Nichol P. Escuadro</p>
+								<p>October 31, 1992</p>
+								<p>West Kamias, QC.</p>
+								<p>0995-008-5674</p>
+								<p>knpescuadro@gmail.com</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	<section id="footer">
+		<div class="grid-container" id="footerSection">
+			<div class="grid-item" id="footerLeft">
+				
+			</div>
+			<div class="grid-item" id="footerRight">
+				<h5>PROFESSIONAL SKILLS</h5>
+				<div class="progress-circle progress-75"><span id="PHP">75</span></div>
+				<div class="progress-circle progress-45"><span id="PHP">45</span></div>
+			</div>
+		</div>
+	</section>
+
+
+</body>
+</html>
